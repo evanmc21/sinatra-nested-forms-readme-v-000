84e8438f871393e06ee5c736fa2e63520c7e84f2
Evan Candler <emcandler21@gmail.com>
2018-04-11 14:49:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,13 +8,13 @@ end
 
 post '/student' do
   @student = Student.new(params[:student])
- 
-  params[:student][:courses].each do |details|
+    #create a new student using the info stored in params[:student]
+  params[:student][:courses].each do |details| #an array containing a series of hashes that each store individual course information.
     Course.new(details)
   end
- 
+  #iterate over params[:student][:courses]
   @courses = Course.all
- 
+
   erb :student
 end
 
